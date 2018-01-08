2464b5203638527bcc5062b224318691bef6e4fc
Geoffrey Matthews <geoffrey.matthews@wwu.edu>
2018-01-08 11:16:12 -0800

    first commit with lectures, labs, syllabus, etc.

---
+++ labs/lab01/comment-demo.rkt
@@ -0,0 +1,16 @@
+#lang racket
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; CSCI 301, Winter 2018
+;; 
+;; Lab #1
+;;
+;; My Name Here
+;; W012345678
+;;
+;; The purpose of this program is to 
+;; bla bla bla
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(provide make-pi)
+
+(define make-pi
+    (lambda (tolerance) 3.1415))
--- lectures/scheme/sicppicture.rkt
+++
@@ -1,8 +0,0 @@
-#lang racket
-
-(#%require sicp-pict)
-
-(paint-hires  (below (beside diagonal-shading
-                             (rotate90 diagonal-shading))
-                     (beside (rotate270 diagonal-shading)
-                             (rotate180 diagonal-shading))))
